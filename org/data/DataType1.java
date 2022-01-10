9c886147989c14da60708f024c7469147e6b4c95
RMuthuPrakash <prakashmuthu216@gmail.com>
2022-01-10 19:48:50 +0530

    code added

---
+++ org/data/DataType1.java
@@ -0,0 +1,16 @@
+package org.data;
+
+public class DataType1 {
+	public static void main(String[] args) {
+		int i=5;
+		String mail="m21@gmail.com";
+		if (i<=1 && mail=="m21@gmail.com") {
+			System.out.println("my id");
+		} else {
+			System.out.println("my mail");
+		}
+
+		}
+		
+		
+}
